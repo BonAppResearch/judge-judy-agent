a5d78096296bb2930725d257bba8ea7da11418c6
Mike <lee.mikemc@gmail.com>
2025-02-06 02:51:54 +0800

    Added UI

---
+++ ui/app/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useWallets, usePrivy } from "@privy-io/react-auth";
+
+export default function Home() {
+  const { wallets } = useWallets();
+  const { connectWallet } = usePrivy();
+  const wallet = wallets[0] || null;
+  const address = wallet?.address || "";
+  const addressConcat =
+    address.substring(0, 4) +
+    "..." +
+    address.substring(address.length - 4, address.length);
+
+  return (
+    <>
+      <nav className="item-center mx-auto flex justify-between p-10">
+        <h1 className="text-4xl font-bold">Judge Judy Agent</h1>
+        <div className="flex">
+          <p>{wallet ? addressConcat : ""}</p>
+          <button
+            className="rounded-xl bg-blue-400 p-4"
+            onClick={wallet ? wallet.disconnect : connectWallet}
+          >
+            {wallet ? "Disconnect" : "Connect Wallet"}
+          </button>
+        </div>
+      </nav>
+      <section className="item-center mx-auto flex justify-between p-10">
+        <div className="item-center flex w-full justify-between">
+          <h2 className="text-xl font-bold">Employer</h2>
+          <button className="rounded-lg bg-green-400 p-4">
+            Request Withdrawal
+          </button>
+        </div>
+      </section>
+      <section className="item-center mx-auto flex justify-between p-10">
+        <div className="item-center flex w-full justify-between">
+          <h2 className="text-xl font-bold">Employee</h2>
+          <button className="rounded-lg bg-green-400 p-4">
+            Request Withdrawal
+          </button>
+        </div>
+      </section>
+    </>
+  );
+}
