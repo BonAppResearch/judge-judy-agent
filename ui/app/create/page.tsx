dd37fcd92fe49a7084e3c3e43802b49e2fe2ddea
Mike <lee.mikemc@gmail.com>
2025-02-08 00:52:28 +0800

    updated resignation checklist layout

--- ui/app/create/page.tsx
+++ ui/app/create/page.tsx
@@ -23,23 +23,118 @@ export default function page() {
       <div className="flex flex-col w-full mx-auto px-10 py-10 items-center gap-10">
         <h1 className="font-bold text-2xl">Create Contract</h1>
         <div className="flex gap-4 w-full justify-start items-center">
-          <p className="font-bold text-lg">Step 1:</p>
+          <p className="font-bold text-lg w-20">Step 1:</p>
           <UploadButton />
         </div>
         <div className="flex w-full gap-4">
-          <p className="font-bold text-lg">Step 2:</p>
-          <div className="flex flex-col gap-4 justify-end items-start">
-            <p>Copy Arbitration Checklist</p>
-            <ul className="">
-              <li>Item 1</li>
-              <li>Item 1</li>
-              <li>Item 1</li>
-              <li>Item 1</li>
-            </ul>
+          <p className="font-bold text-lg w-20">Step 2:</p>
+          <div className="flex flex-col gap-4 justify-end items-start w-full">
+            <p>Review Resignation Checklist</p>
+            <p className="text-xl font-bold border-b-2 border-gray-500">
+              Notice Period
+            </p>
+            <div className="w-full">
+              <div className="flex gap-4">
+                <p className="font-bold">Duration:</p>
+                <p>{/**/}</p>
+              </div>
+              <div className="flex gap-4">
+                <p className="font-bold">Clause Reference:</p>
+                <p>{/**/}</p>
+              </div>
+              <div className="flex gap-4">
+                <p className="font-bold">Exceptions:</p>
+                <p>{/**/}</p>
+              </div>
+              <div className="flex gap-4">
+                <p className="font-bold">Clause Reference:</p>
+                <p>{/**/}</p>
+              </div>
+            </div>
+            <p className="text-xl font-bold border-b-2 border-gray-500">
+              Resignation Letter
+            </p>
+            <div className="w-full">
+              <div className="flex gap-4">
+                <p className="font-bold">Required:</p>
+                <p>{/**/}</p>
+              </div>
+              <div className="flex gap-4">
+                <p className="font-bold">Format:</p>
+                <p>{/**/}</p>
+              </div>
+              <div className="flex gap-4">
+                <p className="font-bold">Required:</p>
+                <p>{/**/}</p>
+              </div>
+              <div className="flex gap-4">
+                <p className="font-bold">Submission Method:</p>
+                <p>{/**/}</p>
+              </div>
+              <div className="flex gap-4">
+                <p className="font-bold">Recipients:</p>
+                <div className="flex flex-col px-5">
+                  <div className="flex">
+                    <p>Primary:</p>
+                    <p>{/**/}</p>
+                  </div>
+                  <div className="flex">
+                    <p>Alternative:</p>
+                    <p>{/**/}</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-4">
+                <p className="font-bold">Deadline:</p>
+                <p>{/**/}</p>
+              </div>
+            </div>
+            <p className="text-xl font-bold border-b-2 border-gray-500">
+              Special Requirements
+            </p>
+            <div className="w-full">
+              <div className="flex gap-4">
+                <p className="font-bold">Requirement:</p>
+                <p>{/**/}</p>
+              </div>
+              <div className="flex gap-4">
+                <p className="font-bold">Deadline:</p>
+                <p>{/**/}</p>
+              </div>
+              <div className="flex gap-4">
+                <p className="font-bold">Clause Reference:</p>
+                <p>{/**/}</p>
+              </div>
+            </div>
+            <p className="text-xl font-bold border-b-2 border-gray-500">
+              Post Resignation Requirements
+            </p>
+            <div className="w-full">
+              <div className="flex gap-4">
+                <p className="font-bold">Obligation:</p>
+                <p>{/**/}</p>
+              </div>
+              <div className="flex gap-4">
+                <p className="font-bold">Duration:</p>
+                <p>{/**/}</p>
+              </div>
+              <div className="flex gap-4">
+                <p className="font-bold">Clause Reference:</p>
+                <p>{/**/}</p>
+              </div>
+            </div>
+            <p className="text-xl font-bold border-b-2 border-gray-500">
+              Compliance Consequences
+            </p>
+            <div className="w-full">
+              <div className="flex gap-4">
+                <p>{/**/}</p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="flex w-full gap-4">
-          <p className="font-bold text-lg">Step 3:</p>
+          <p className="font-bold text-lg w-20">Step 3:</p>
           <div className="flex flex-col">
             <label for="employeeWalletAdd">
               Enter Employee's Wallet Address
@@ -52,7 +147,7 @@ export default function page() {
           </div>
         </div>
         <div className="flex w-full gap-4">
-          <p className="font-bold text-lg">Step 4:</p>
+          <p className="font-bold text-lg w-20">Step 4:</p>
           <div className="flex flex-col">
             <p>Sign Transaction to confirm the contract</p>
             <button className="bg-blue-300 rounded-lg p-2 w-[50%] text-xs">
@@ -61,7 +156,7 @@ export default function page() {
           </div>
         </div>
         <div className="flex w-full gap-4">
-          <p className="font-bold text-lg">Step 5:</p>
+          <p className="font-bold text-lg w-20">Step 5:</p>
           <div className="flex flex-col">
             <button className="bg-blue-300 rounded-lg p-2 w-[90%] text-xs">
               Deposit ETH
