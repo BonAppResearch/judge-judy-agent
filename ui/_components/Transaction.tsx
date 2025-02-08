5fb0ef93f35bc22d16245964605226941337cde6
Mike <lee.mikemc@gmail.com>
2025-02-08 13:58:25 +0800

    fixed merge conflicts

--- ui/_components/Transaction.tsx
+++ ui/_components/Transaction.tsx
@@ -11,7 +11,6 @@ type TxProp = {
   txStatus: string;
 };
 
-<<<<<<< HEAD
 export default function Transaction({
   role = "employer",
   createdDate,
@@ -19,9 +18,6 @@ export default function Transaction({
   txHash,
   txStatus,
 }: TxProp) {
-=======
-export default function Transaction({ createdDate, walletAddress, txHash, txStatus }: TxProp) {
->>>>>>> safe-wallet
   return (
     <div className="p-4 flex justify-between border-b-2">
       <div className="flex gap-10 w-full items-center">
@@ -30,24 +26,22 @@ export default function Transaction({ createdDate, walletAddress, txHash, txStat
         <p>{txHash}</p>
         <p>{txStatus}</p>
       </div>
-<<<<<<< HEAD
-      <div className="flex gap-4">
-        <button className="rounded-lg bg-green-400 px-4 p-2 text-xs">
-          Request Withdrawal
-        </button>
-      </div>
-=======
       <button
         className="rounded-lg bg-green-400 px-4 py-2 text-xs"
         onClick={() => {
           console.log(window.ethereum);
-          const client = createWalletClient({ chain: baseSepolia, transport: custom(window.ethereum) });
-          createSafeClient(client, "0x0000000000000000000000000000000000000000");
+          const client = createWalletClient({
+            chain: baseSepolia,
+            transport: custom(window.ethereum),
+          });
+          createSafeClient(
+            client,
+            "0x0000000000000000000000000000000000000000"
+          );
         }}
       >
         Request Withdrawal
       </button>
->>>>>>> safe-wallet
     </div>
   );
 }
