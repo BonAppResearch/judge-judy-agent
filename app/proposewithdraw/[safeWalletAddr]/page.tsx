e9761eb46833f7a92d8a526f35ac9efbb87edc28
Mike <lee.mikemc@gmail.com>
2025-02-09 11:51:27 +0800

    remove proposewithdrawal methods

--- app/proposewithdraw/[safeWalletAddr]/page.tsx
+++ app/proposewithdraw/[safeWalletAddr]/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
-import { useParams } from "next/navigation";
-import { proposeWithdrawTransaction } from "@/app/utils/safeHelper";
+// import { useParams } from "next/navigation";
+// import { proposeWithdrawTransaction } from "@/app/utils/safeHelper";
 
 export default function page() {
-  const params = useParams();
-  const { safeWalletAddr } = params;
+  // const params = useParams();
+  // const { safeWalletAddr } = params;
   return <div></div>;
 }
